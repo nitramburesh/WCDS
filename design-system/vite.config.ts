13d09ab31a08a44f37034d7a1413bbf897d660a9
Martin Bures <71663069+nitramburesh@users.noreply.github.com>
2025-07-03 22:34:16 +0200

    vite config

---
+++ design-system/vite.config.ts
@@ -0,0 +1,58 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "wcds",
+      formats: ["es"],
+      fileName: "wcds.js",
+    },
+    rollupOptions: {
+      external: ["lit"],
+      output: {
+        globals: {
+          lit: "lit",
+        },
+      },
+    },
+    copyPublicDir: false,
+  },
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
+  },
+});
